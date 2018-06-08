af942262dc599adec0d986a24691c3c23d81c111
Robert Klim <kontakt.robert.klim@gmail.com>
2018-06-08 13:34:45 +0200

    Setup 'static' directory

--- src/todolist/urls.py
+++ src/todolist/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 from django.views.generic import TemplateView
 
@@ -22,3 +24,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', TemplateView.as_view(template_name='home.html')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
