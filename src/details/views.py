cc4020b116600a11a588c10d5943c388ac6c69b0
Robert Klim <kontakt.robert.klim@gmail.com>
2018-06-15 10:33:28 +0200

    Create basic CRUD views in 'details' app

--- src/details/views.py
+++ src/details/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+
+from .forms import DetailsForm
+from .models import Details
 
 # Create your views here.
+
+class DetailsListView(ListView):
+    def get_queryset(self):
+        return Details.objects.filter(user=self.request.user)
+
+class DetailsDetailView(DetailView):
+    def get_queryset(self):
+        return Details.objects.filter(user=self.request.user)
+
+class DetailsCreateView(CreateView):
+    form_class = DetailsForm
+    def get_queryset(self):
+        return Details.objects.filter(user=self.request.user)
+
+class DetailsUpdateView(UpdateView):
+    form_class = DetailsForm
+    def get_queryset(self):
+        return Details.objects.filter(user=self.request.user)
