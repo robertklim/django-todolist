267699dcb008c4f75990bee0aaeab187bdd8a1b9
Robert Klim <kontakt.robert.klim@gmail.com>
2018-06-15 09:52:09 +0200

    Create 'details' app model and migration

--- src/details/models.py
+++ src/details/models.py
@@ -1,3 +1,21 @@
+from django.conf import settings
 from django.db import models
 
+from todos.models import Todos
+
 # Create your models here.
+
+class Details(models.Model):
+    # associations
+    user        = models.ForeignKey(settings.AUTH_USER_MODEL)
+    todo        = models.ForeignKey(Todos)
+    # details data
+    keywords    = models.TextField(help_text='Separete each bo comma')
+    description = models.TextField(blank=True, null=True, help_text='Separete each bo comma')
+    public      = models.BooleanField(default=True)
+    timestamp   = models.DateField(auto_now_add=True)
+    updated     = models.DateField(auto_now=True)
+
+    # Default ordering
+    class Meta:
+        ordering = ['-updated', '-timestamp']
