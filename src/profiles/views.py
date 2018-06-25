c2fea4cc59c67ab1da9e7ce8df6e9ad5b5b9dd98
Robert Klim <kontakt.robert.klim@gmail.com>
2018-06-25 15:09:24 +0200

    Add searching on user's profile page

--- src/profiles/views.py
+++ src/profiles/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
 # Create your views here.
 
+from details.models import Details
+from todos.models import Todos
+
 User = get_user_model()
 
 class ProfileDetailView(DetailView):
@@ -14,3 +17,13 @@ class ProfileDetailView(DetailView):
         if username is None:
             raise Http404
         return get_object_or_404(User, username__iexact=username, is_active=True)
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProfileDetailView, self).get_context_data(*args, **kwargs)
+        user = context['user']
+        search_query = self.request.GET.get('q')
+        qs = Todos.objects.filter(user=user).search(search_query)
+
+        if qs.exists():
+            context['todos'] = qs
+        return context
--- src/todos/models.py
+++ src/todos/models.py
@@ -1,11 +1,32 @@
 from django.conf import settings
 from django.db import models
+from django.db.models import Q
 from django.core.urlresolvers import reverse
 from .validators import validate_expires
 
 # Create your models here.
 
 User = settings.AUTH_USER_MODEL
+
+class TodosQuerySet(models.query.QuerySet):
+    def search(self, query):    # Todos.objects.all().search(query)
+        if query:
+            query = query.strip()
+            return self.filter(
+                Q(title__icontains=query)|
+                Q(entry__icontains=query)|
+                Q(details__description__icontains=query)|
+                Q(details__keywords__icontains=query)
+                ).distinct()
+        else:
+            return self
+
+class TodosManager(models.Manager):
+    def get_queryset(self):
+        return TodosQuerySet(self.model, using=self._db)
+
+    def search(self, query):    # Todos.objects.search(query)
+        return self.get_queryset().filter(title__icontains=query)
 
 class Todos(models.Model):
     user        = models.ForeignKey(User)
@@ -14,6 +35,8 @@ class Todos(models.Model):
     expires     = models.DateField(null=True, blank=True, validators=[validate_expires])
     timestamp   = models.DateField(auto_now_add=True)
     updated     = models.DateField(auto_now=True)
+
+    objects = TodosManager()
 
     def __str__(self):
         return self.title
