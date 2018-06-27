3c12f8a2026cfc5923f74bc98c81d9a70a0c260c
Robert Klim <kontakt.robert.klim@gmail.com>
2018-06-27 10:29:05 +0200

    Create 'Profile' model and register it in admin's panel

--- src/profiles/models.py
+++ src/profiles/models.py
@@ -1,3 +1,23 @@
+from django.conf import settings
 from django.db import models
-
+from django.db.models.signals import post_save
 # Create your models here.
+
+User = settings.AUTH_USER_MODEL
+
+class Profile(models.Model):
+    user        = models.OneToOneField(User) # user.profile
+    followers   = models.ManyToManyField(User, related_name='followers', blank=True) # user.profile_set.all()
+    following   = models.ManyToManyField(User, related_name='following', blank=True) # user.profile_set.all()
+    activated   = models.BooleanField(default=False)
+    timestamp   = models.DateTimeField(auto_now_add=True)
+    updated     = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.username
+
+def post_save_user_receiver(sender, instance, created, *args, **kwargs):
+    if created:
+        profile, is_created = Profile.object.get_or_create(user=instance)
+
+post_save.connect(post_save_user_receiver, sender=User)
