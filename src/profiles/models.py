50f7c05fbc2b3ca40b74d870aebd2ca323f50476
Robert Klim <kontakt.robert.klim@gmail.com>
2018-06-27 11:09:47 +0200

    Add default followers

--- src/profiles/models.py
+++ src/profiles/models.py
@@ -18,6 +18,9 @@ class Profile(models.Model):
 
 def post_save_user_receiver(sender, instance, created, *args, **kwargs):
     if created:
-        profile, is_created = Profile.object.get_or_create(user=instance)
+        profile, is_created = Profile.objects.get_or_create(user=instance)
+        default_user_profile = Profile.objects.get_or_create(user__id=1)[0]
+        default_user_profile.followers.add(instance)
+        profile.followers.add(default_user_profile.user)
 
 post_save.connect(post_save_user_receiver, sender=User)
